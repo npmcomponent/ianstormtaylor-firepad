a16e7173500046b002af45f738671c2bf0d8fc3b
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-08-24 19:42:19 -0700

    wow dumb

--- firepad.js
+++ firepad.js
@@ -4014,7 +4014,6 @@ firepad.Firepad = (function(global) {
   var ATTR = firepad.AttributeConstants;
   var utils = firepad.utils;
   var LIST_TYPE = firepad.LineFormatting.LIST_TYPE;
-  var CodeMirror = CodeMirror;
 
   function Firepad(ref, place, options) {
     if (!(this instanceof Firepad)) { return new Firepad(ref, place, options); }
